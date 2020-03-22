65c0f04246fb896628369a44c0302c6da6aec167
Vladimir Jimenez <allejo@me.com>
2020-03-21 21:10:08 -0700

    Introduce dark mode support

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -1,25 +1,25 @@
-$(function(){
+$(function() {
     $('html').toggleClass('no-js js');
 
     // Update sidebar highlighting based on Scrollspy
     $(window).on('activate.bs.scrollspy', function () {
-        var spyTarget = $('[data-spy="scroll"]').data('target');
-        var $activeSpy = $(spyTarget).find('.nav-link.active');
-        var $tree = $activeSpy.parentsUntil('.bs-docs-sidenav', 'li');
+        const spyTarget = $('[data-spy="scroll"]').data('target');
+        const $activeSpy = $(spyTarget).find('.nav-link.active');
+        const $tree = $activeSpy.parentsUntil('.bs-docs-sidenav', 'li');
 
         $tree.find('> a').addClass('active');
     });
 
     // Toggleable mobile table of contents button
     $('.toggle-toc').on('click', function () {
-        var $this = $(this);
+        const $this = $(this);
 
         $this
             .parents('.mobile-toc')
             .find('.js-toc')
             .toggle();
 
-        var $btn = $this.find('[data-role="toggle"]');
+        const $btn = $this.find('[data-role="toggle"]');
 
         if ($btn.text() === 'Hide') {
             $btn.text('Show');
@@ -30,10 +30,10 @@ $(function(){
 
     // Make the triangular pattern in the header
     if (uiColors) {
-        var $masthead = $('.site-masthead');
+        const $masthead = $('.site-masthead');
 
         if ($masthead.length) {
-            var t = new Trianglify({
+            const t = new Trianglify({
                 cellsize: 90,
                 noiseIntensity: 0,
                 x_gradient: [
@@ -41,9 +41,9 @@ $(function(){
                     uiColors[1],
                 ],
             });
-            var pattern = t.generate(window.screen.width | $masthead.outerWidth(), $masthead.outerHeight() * 1.2);
+            const pattern = t.generate(window.screen.width | $masthead.outerWidth(), $masthead.outerHeight() * 1.2);
 
-            var style = $('<style>.site-masthead { background-image: ' + pattern.dataUrl + '; }</style>');
+            const style = $('<style>.site-masthead { background-image: ' + pattern.dataUrl + '; }</style>');
             $('html > head').append(style);
         }
     }
